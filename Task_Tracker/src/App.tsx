d74b702eac343666d4606ea2ad6f9d0f90c0b884
Rehumile <sekotorehumile1@gmail.com>
2023-11-22 21:42:08 +0200

    Added code that will add a task to the list when user clicks button and will cross out item once complete

---
+++ Task_Tracker/src/App.tsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+ import { Header } from './components/Header'
+ import { ToDoList } from './components/TodoList'
+
+
+function App() {
+ // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <ToDoList/>
+    </>
+  )
+}
+
+export default App
