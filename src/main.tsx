d97c849cb7a1807e819f809db94d704c4a6964bc
unknown <sumimaya13@gmail.com>
2023-12-13 11:54:52 +0200

    updated import links

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './src/App'
-import "./src/scss/app.scss"
-import "./src/scss/header.scss"
-import "./src/scss/PriorityLevel.scss"
+import App from '../src/App'
+import "../src/scss/app.scss"
+import "../src/scss/header.scss"
+import "../src/scss/PriorityLevel.scss"
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
